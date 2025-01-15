7438389dde53cd5a7d126e33bc875b4801ee04da
Luca Vieira <lucamevieira@gmail.com>
2025-01-15 11:09:19 -0300

    feat: Including invoices to appear in customer query

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -17,16 +17,18 @@ class CustomerController extends Controller
     public function index(Request $request)
     {
         $filter = new CustomersFilter();
-        $queryItems = $filter->transform($request); //(['column', 'operator','value'])
+        $filterItems = $filter->transform($request); //(['column', 'operator','value'])
 
-        if (count($queryItems) == 0){
-            return new CustomerCollection(Customer::paginate());
-        }
-        else{
-            $customers = Customer::where($queryItems)->paginate();
+        $includeInvoices = $request->query('includeInvoices');
 
-            return new CustomerCollection($customers->appends($request->query()));
+        $customers = Customer::where($filterItems);
+
+        if($includeInvoices){
+            $customers = $customers->with('invoices');
         }
+
+        return new CustomerCollection($customers->paginate()->appends($request->query()));
+
     }
 
     /**
@@ -50,6 +52,12 @@ class CustomerController extends Controller
      */
     public function show(Customer $customer)
     {
+        $includeInvoices = request()->query('includeInvoices');
+
+        if($includeInvoices){
+            return new CustomerResource($customer->loadMissing('invoices'));
+        }
+
         return new CustomerResource($customer);
     }
 
